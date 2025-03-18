644c9eb7749858e00c384a42cd2dfb99cfe0e3e6
KBM05haruin30 <wjdrbs1189@gmail.com>
2025-03-18 23:17:09 +0900

    feat : 유저 entity 생성

---
+++ src/main/java/pokemon/splender/domain/member/entity/Member.java
@@ -0,0 +1,39 @@
+package pokemon.splender.domain.member.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "member")
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long memberId;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    protected Member() {
+
+    }
+
+    public Member(String name, String email) {
+        this.name = name;
+        this.email = email;
+        this.createdAt = LocalDateTime.now();
+    }
+
+
+}
