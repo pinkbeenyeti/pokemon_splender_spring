c02bd60b431a8cbbc873c7ba5c7c8022dbbfa1c8
pinkbeenyeti <rbgusdn12@gmail.com>
2025-03-25 18:55:33 +0900

    feat: 사용자 정보를 저장하는 엔티티 생성
    body: id, provider, providerId, createdAt, lastNameChangeAt으로 이루어짐. 해당 유저는 OAuth2 인증 시 provider와 providerId를 기준으로 검증함.

---
+++ src/main/java/pokemon/splender/user/entity/User.java
@@ -0,0 +1,76 @@
+package pokemon.splender.user.entity;
+
+import static lombok.AccessLevel.PROTECTED;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import java.time.LocalDateTime;
+import java.util.UUID;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import pokemon.splender.exception.CustomException;
+
+@Entity
+@Table(name = "users", uniqueConstraints = {
+    @UniqueConstraint(name = "unique_user_provider_id", columnNames = {"providerId", "provider"})
+})
+@NoArgsConstructor(access = PROTECTED)
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter
+    private Long id;
+
+    @Getter
+    @Column(nullable = false)
+    private String name;
+
+    @Getter
+    @Column(nullable = false)
+    private String providerId;
+
+    @Getter
+    @Column(nullable = false)
+    private String provider;
+
+    @Getter
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @Getter
+    @Column(nullable = false)
+    private LocalDateTime lastNameChangedAt;
+
+    public User(String providerId, String provider) {
+        LocalDateTime now = LocalDateTime.now();
+
+        this.name = createRandomName();
+        this.providerId = providerId;
+        this.provider = provider;
+        this.createdAt = now;
+        this.lastNameChangedAt = now.minusMonths(2);
+    }
+
+    public void updateName(String name) {
+        if (!canChaneName(name)) {
+            throw CustomException.invalidNameChangePeriod();
+        } else {
+            this.name = name;
+            this.lastNameChangedAt = LocalDateTime.now();
+        }
+    }
+
+    private boolean canChaneName(String name) {
+        return lastNameChangedAt.isBefore(LocalDateTime.now().minusDays(30));
+    }
+
+    private String createRandomName() {
+        return "User_" + UUID.randomUUID().toString().substring(0, 8);
+    }
+}
