04218b1089b4bc8814a78a28121d8c54260d4a4b
pinkbeenyeti <rbgusdn12@gmail.com>
2025-03-25 18:52:04 +0900

    feat: jwt 인증을 처리하는 커스텀 필터 생성
    body: {도메인}/api 요청일 경우 jwt 토큰 검증을 수행하는 필터

---
+++ src/main/java/pokemon/splender/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,51 @@
+package pokemon.splender.jwt;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import pokemon.splender.exception.CustomException;
+import pokemon.splender.jwt.util.JwtUtil;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtUtil jwtUtil;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+        FilterChain filterChain) throws ServletException, IOException {
+        String requestURI = request.getRequestURI();
+
+        if (isApiPath(requestURI)) {
+            handleTokenAuthentication(request, response);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private boolean isApiPath(String requestURI) {
+        return requestURI.startsWith("/api/");
+    }
+
+    private void handleTokenAuthentication(HttpServletRequest request,
+        HttpServletResponse response) {
+        String token = jwtUtil.extractToken(request);
+
+        if (jwtUtil.validateToken(token)) {
+            Long userId = Long.valueOf(jwtUtil.getSubject(token));
+            UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(
+                userId, null, null);
+            SecurityContextHolder.getContext().setAuthentication(auth);
+        } else {
+            throw CustomException.invalidTokenException();
+        }
+    }
+}
