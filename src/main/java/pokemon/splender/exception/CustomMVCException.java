c87a4dbb7cbe5d098a66a16bdc9dc020974b2612
pinkbeenyeti <rbgusdn12@gmail.com>
2025-03-27 23:27:03 +0900

    fix: MVC에서 발생하는 예외를 처리하는 핸들러로 명시 및 수정

---
+++ src/main/java/pokemon/splender/exception/CustomMVCException.java
@@ -0,0 +1,20 @@
+package pokemon.splender.exception;
+
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static pokemon.splender.exception.ErrorMessage.INVALID_NAME_CHANGE_PERIOD;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public class CustomMVCException extends RuntimeException {
+
+    private final ErrorMessage errorMessage;
+    private final HttpStatus status;
+
+    public static CustomFilterException invalidNameChangePeriod() {
+        return new CustomFilterException(INVALID_NAME_CHANGE_PERIOD, BAD_REQUEST);
+    }
+}
