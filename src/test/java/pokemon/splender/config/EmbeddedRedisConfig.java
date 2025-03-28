c839d90233c0aa98f38ddff41595d4191a1f962d
KBM05haruin30 <wjdrbs1189@gmail.com>
2025-03-28 14:19:05 +0900

    test : Embedded Redis 설정 추가 테스트 환경용 EmbeddedRedisConfig 구현

---
+++ src/test/java/pokemon/splender/config/EmbeddedRedisConfig.java
@@ -0,0 +1,31 @@
+package pokemon.splender.config;
+
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import java.io.IOException;
+import org.springframework.boot.test.context.TestConfiguration;
+import redis.embedded.RedisServer;
+
+@TestConfiguration
+public class EmbeddedRedisConfig {
+
+    private RedisServer redisServer;
+
+    public EmbeddedRedisConfig() throws IOException {
+        this.redisServer = new RedisServer(6379);
+    }
+
+    @PostConstruct
+    public void start() {
+        redisServer.start();
+        System.out.println("Embedded Redis Server started on port 6379");
+    }
+
+    @PreDestroy
+    public void stop() {
+        redisServer.stop();
+        System.out.println("Embedded Redis Server stopped");
+    }
+
+}
