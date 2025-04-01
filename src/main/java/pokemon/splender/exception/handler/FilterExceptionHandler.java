b34de9bc089947d7314bd33409cdb4a9e25292b3
KBM05haruin30 <wjdrbs1189@gmail.com>
2025-04-01 13:22:23 +0900

    refactor : 쿠키 삭제 로직 위치 수정 토큰에 대한 예외는 필터에서 발생하므로 MVC까지 전달되지 않으므로 GlobalMVCExceptionHandler에 있던 쿠키 삭제 로직을 FilterExceptionHandler로 옮긴다.

--- src/main/java/pokemon/splender/exception/handler/FilterExceptionHandler.java
+++ src/main/java/pokemon/splender/exception/handler/FilterExceptionHandler.java
@@ -7,10 +7,12 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 import pokemon.splender.exception.CustomFilterException;
 import pokemon.splender.exception.ErrorMessage;
+import pokemon.splender.jwt.util.TokenCookieUtil;
 
 @Component
 @RequiredArgsConstructor
@@ -39,6 +41,13 @@ public class FilterExceptionHandler extends OncePerRequestFilter {
         // 문자 인코딩 설정 (UTF-8)
         response.setCharacterEncoding("UTF-8");
         response.setContentType("application/json;charset=UTF-8");
+
+        // 만료된 토큰이거나 탈취된 토큰일 경우 쿠키 삭제
+        if (ex.getStatus() == HttpStatus.FORBIDDEN
+            || ex.getStatus() == HttpStatus.UNAUTHORIZED) {
+            response.addHeader("Set-Cookie", TokenCookieUtil.deleteAccessTokenCookie().toString());
+            response.addHeader("Set-Cookie", TokenCookieUtil.deleteRefreshTokenCookie().toString());
+        }
 
         // ObjectMapper를 사용하여 ErrorMessage를 JSON으로 변환
         String jsonResponse = objectMapper.writeValueAsString(errorMessage);
--- src/main/java/pokemon/splender/exception/handler/GlobalMVCExceptionHandler.java
+++ src/main/java/pokemon/splender/exception/handler/GlobalMVCExceptionHandler.java
@@ -1,31 +1,19 @@
 package pokemon.splender.exception.handler;
 
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import pokemon.splender.exception.CustomMVCException;
 import pokemon.splender.exception.ErrorMessage;
-import pokemon.splender.jwt.util.TokenCookieUtil;
 
 @RestControllerAdvice
 public class GlobalMVCExceptionHandler {
 
     @ExceptionHandler(CustomMVCException.class)
     public ResponseEntity<ErrorMessage> handleCustomException(CustomMVCException customException) {
-        HttpHeaders headers = new HttpHeaders();
-
-        // 만료된 토큰이거나 탈취된 토큰일 경우 쿠키 삭제
-        if (customException.getStatus() == HttpStatus.FORBIDDEN
-            || customException.getStatus() == HttpStatus.UNAUTHORIZED) {
-            headers.add("Set-Cookie", TokenCookieUtil.deleteAccessTokenCookie().toString());
-            headers.add("Set-Cookie", TokenCookieUtil.deleteRefreshTokenCookie().toString());
-        }
 
         return new ResponseEntity<>(
-            customException.getErrorMessage(), // body : ErrorMessage enum 값
-            headers,                           // headers : 쿠키 삭제 정보 포함
-            customException.getStatus());      // status
+            customException.getErrorMessage(),
+            customException.getStatus());
     }
 }
