30b77aad92ce05d9fcfd23b1af9a9d05c63cb697
KBM05haruin30 <wjdrbs1189@gmail.com>
2025-04-08 15:51:09 +0900

    feat : Path 별 이미지 찾기 구현

--- src/main/java/pokemon/splender/image/repository/ImageRepository.java
+++ src/main/java/pokemon/splender/image/repository/ImageRepository.java
@@ -7,4 +7,9 @@ import pokemon.splender.image.entity.Image;
 public interface ImageRepository extends JpaRepository<Image, Long> {
 
     List<Image> findByIdIn(List<Long> ids);
+
+    // Path(폴더)별로 이미지 찾기
+    // 다음과 같은 쿼리 의미를 가짐
+    // SELECT i FROM Image i WHERE i.path LIKE CONCAT(:basePath, '%')
+    List<Image> findAllByPathStartingWith(String basePath);
 }
