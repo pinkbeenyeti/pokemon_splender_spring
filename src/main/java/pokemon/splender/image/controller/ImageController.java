ca63086c5717da057d2fb39d191f73888bfbf316
KBM05haruin30 <wjdrbs1189@gmail.com>
2025-04-09 16:52:39 +0900

    remove : 불필요한 import 삭제

--- src/main/java/pokemon/splender/image/controller/ImageController.java
+++ src/main/java/pokemon/splender/image/controller/ImageController.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
