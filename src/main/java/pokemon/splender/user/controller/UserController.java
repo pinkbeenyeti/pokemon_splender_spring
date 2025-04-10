cb2ca690feebf1f318eb0bb592c1d1a9ca374b3b
pinkbeenyeti <rbgusdn12@gmail.com>
2025-04-10 21:08:43 +0900

    feat: 이름 변경 api 추가

--- src/main/java/pokemon/splender/user/controller/UserController.java
+++ src/main/java/pokemon/splender/user/controller/UserController.java
@@ -1,13 +1,31 @@
 package pokemon.splender.user.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pokemon.splender.response.CustomResponse;
+import pokemon.splender.user.dto.UserNameEditRequest;
+import pokemon.splender.user.service.UserService;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("api/v1/user")
 public class UserController {
-    
+
+    private final UserService userService;
+
+    @PostMapping("/name/edit")
+    public ResponseEntity<?> editUserName(
+        Authentication authentication,
+        @RequestBody UserNameEditRequest request) {
+
+        Long userId = (Long) authentication.getPrincipal(); // userId 그대로 꺼내기
+        userService.updateUserName(userId, request.newName());
+
+        return CustomResponse.ok();
+    }
 }
