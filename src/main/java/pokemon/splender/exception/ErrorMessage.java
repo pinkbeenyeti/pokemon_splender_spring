0789320f075df2f82ff061b6db8a42c5207061cf
pinkbeenyeti <rbgusdn12@gmail.com>
2025-04-10 21:12:21 +0900

    feat: 이름 변경 시 발생하는 예외 메서드 및 에러 메시지 정의

--- src/main/java/pokemon/splender/exception/ErrorMessage.java
+++ src/main/java/pokemon/splender/exception/ErrorMessage.java
@@ -14,7 +14,16 @@ public enum ErrorMessage {
     NOT_EXIST_ACCESS_COOKIE(-10001, "엑세스 토큰 쿠키가 존재하지 않습니다."),
     NOT_EXIST_REFRESH_COOKIE(-10002, "리프레시 토큰 쿠키가 존재하지 않습니다."),
     NOT_EXIST_THAT_COOKIE(-10003, "해당 쿠키가 존재하지 않습니다."),
-    INVALID_NAME_CHANGE_PERIOD(-10010, "이름을 변경한 지 30일이 지나지 않았습니다."),
+
+    //user 관련 문제
+    USER_NOT_FOUND(-10010, "해당 유저가 존재하지 않습니다."),
+    NICKNAME_REQUIRED(-10020, "닉네임을 입력하지 않았습니다."),
+    INVALID_NICKNAME_LENGTH(-10021, "닉네임은 2글자 이상 8글자 이하여야 합니다."),
+    INVALID_NICKNAME_FORMAT(-10022, "닉네임에는 특수문자/공백/초성을 입력할 수 없습니다. 한글 단어, 영어, 숫자만 입력하세요."),
+    INVALID_NICKNAME_FORMAT_ONLY_NUMBER(-10023, "닉네임은 숫자만 입력할 수 없습니다."),
+    INVALID_NICKNAME_FORMAT_NOT_EMOTICON(-10024, "닉네임은 이모티콘을 포함할 수 없습니다."),
+    DUPLICATED_NICKNAME(-10025, "중복된 닉네임입니다."),
+    INVALID_NAME_CHANGE_PERIOD(-10026, "닉네임을 변경한 지 30일이 지나지 않았습니다."),
 
     //oauth 인증 문제
     INVALID_OAUTH_FORMAT(-10100, "서버 오류, OAuth2 인증시 요청 형식이 잘못 되었습니다."),
@@ -24,13 +33,14 @@ public enum ErrorMessage {
     INVALID_TOKEN(-10200, "유효하지 않은 토큰입니다."),
     EXPIRED_TOKEN(-10201, "만료된 토큰입니다."),
 
-
     // image 관련 문제
     INVALID_IMAGE(-10300, "유효하지 않은 이미지입니다."),
-    NOT_EXIST_IMAGE(-10301, "이미지가 존재하지 않습니다.");
+    NOT_EXIST_IMAGE(-10301, "이미지가 존재하지 않습니다."),
 
-
+    //서버 내부 문제, 해당 에러 메시지 찾을 수 없음
+    SERVER_INTERNAL_MESSAGE_ERROR(-1, "서버 내부 오류입니다.");
 
     private final int code;
     private final String message;
+
 }
